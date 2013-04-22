11dcb70602b9da71075c1f683196a3294ab6aea0
Greg Wang <gregwym@gmail.com>
2013-04-22 17:28:58 -0400

    Initial Commit

---
+++ Cocos2dTrail/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Cocos2dTrail
+//
+//  Created by Greg Wang on 4/22/13.
+//  Copyright Greg Wang 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Cocos2dTrail/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2dTrail
+//
+//  Created by Greg Wang on 4/22/13.
+//  Copyright Greg Wang 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
